120d4ce3321e4f720aa8f79bc017079e96bf2085
ColdSyntax <syntaxcold64@gmail.com>
2024-10-23 16:35:19 -0400

    Finish writing Main.java, needs comments

---
+++ src/ping/Main.java
@@ -0,0 +1,13 @@
+package ping;
+
+import ping.Options;
+import ping.PingBot;
+
+public class Main
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        PingBot breaker = new PingBot(Options.Builder.of(args));
+        //breaker.launch();
+    }
+}
