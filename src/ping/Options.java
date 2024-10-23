52b9bce233144a5326a2a7132c8b730e9067df1b
ColdSyntax <syntaxcold64@gmail.com>
2024-10-23 17:12:37 -0400

    Finish Option.java

--- src/ping/Options.java
+++ src/ping/Options.java
@@ -1,4 +1,65 @@
 package ping;
 
-public class Options {
+import java.util.HashMap;
+import java.util.Map;
+public class Options
+{
+    private final Map<String, Object> options = new HashMap<String, Object>();
+
+    public boolean isOption(String id)
+    {
+        return this.options.containsKey(id);
+    }
+
+    public <T> T getOption(String id, T defaultValue)
+    {
+        return (T)this.options.getOrDefault(id, defaultValue);
+    }
+
+    public Map<String, Object> getOptions()
+    {
+        return this.options;
+    }
+
+    public static class Builder
+    {
+        public static Options of(String[] args)
+        {
+            Options options = new Options();
+            String[] arrstring = args;
+            int arguments = args.length;
+
+            for (int i = 0; i < arguments; i++)
+            {
+                String arg = arrstring[i];
+                String[] part = arg.split("-");
+
+                if(part.length <= 1)
+                    continue;
+
+                Object value = part[1];
+
+                try
+                {
+                    value = Integer.parseInt((String) value);
+                }
+                catch (Exception exception)
+                {}
+
+                if(value instanceof String && ((String) value).equals("true"))
+                {
+                    try
+                    {
+                        value = Boolean.parseBoolean((String) value);
+                    }
+                    catch (Exception exception)
+                    {}
+                }
+
+                options.options.put(part[0].replaceAll("-", arg), value);
+            }
+
+            return options;
+        }
+    }
 }
